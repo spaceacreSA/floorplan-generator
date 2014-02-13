5a99b5084313a3d5f3a18bb49d4c338002fb5667
maartenhemmes <maartenhemmes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-13 02:27:03 +0000

    Initial commit
    git-svn-id: https://plugins.svn.wordpress.org/floorplan-generator/tags/0.1@856848 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/settings.class.php
@@ -0,0 +1,55 @@
+<?php 
+class floorplan_settings{
+	function __construct()
+	{
+		#SET CUSTOM POST TYPE
+		add_action( 'init', 'create_post_type' );
+		function create_post_type()
+		{
+			register_post_type(
+			'floorplans',
+			array
+			(
+			'labels' => array(
+			'name' => __('Floorplans'),
+			'singular_name' => __('Floorplan'),
+			'edit_item' => __('Edit Floorplan'),
+			'new_item' => __('New Floorplan'),
+			'view_item' => __('View Floorplan'),
+			'search_items' => __('Search Floorplans')),
+			'public' => true,
+			'show_ui' => true,
+			'capability_type' => 'post',
+			'hierarchical' => false,
+			'rewrite' => array(
+			'slug' => 'floorplans',
+			'with_front' => false),
+			'has_archive' => true,
+			'query_var' => true,
+			'menu_position' => 25,
+			'supports' => array('title','excerpt', 'author','editor','thumbnail','excerpt','custom-fields','revisions','comments','page-attributes')
+			)
+			);
+		}
+		
+		##SET CATEGORIES FOR CUSTOM POST TYPE
+	//	register_taxonomy("Office", array("floorplans"));
+		
+		
+	/*	function remove_metaboxes(){
+			remove_meta_box('slugdiv', 'obpcontacts', 'normal'); // Slug
+			remove_meta_box('submitdiv', 'obpcontacts', 'side'); // Publish box
+		}
+		add_action( 'add_meta_boxes', 'remove_metaboxes', 11 );*/
+		
+		##INIT DIRECTORY FOR UPLOADING PDF##
+		$dir_name = WP_CONTENT_DIR.'/uploads/floorplans/';
+		if ( ! is_dir($dir_name) )
+		{
+			wp_mkdir_p($dir_name) or die("Could not create Floorplan Generator directory " . $dir_name);
+		}
+		
+		
+	}
+	
+}
