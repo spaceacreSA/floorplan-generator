4d949412a1d46cf6ad7a4a4ee8ae9f731b6feddc
maartenhemmes <maartenhemmes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-19 01:14:47 +0000

    Fixed bug regarding to saving post Added backlinks
    git-svn-id: https://plugins.svn.wordpress.org/floorplan-generator/tags/0.2.3.1@860579 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ floorplan-generator-plugin.php
@@ -0,0 +1,27 @@
+<?php
+/*
+Plugin Name: Floorplan
+Plugin URI: http://www.wptooling.com
+Description: Upload a floorplan and add position markers to show (room) images.
+Version: 0.2.3.1
+Author: Maarten Hemmes - WPTooling
+Author URI: http://www.wptooling.com
+License: A "Slug" license name e.g. GPL2
+*/
+
+//ini_set( 'display_errors', 1 );
+//error_reporting( E_ERROR | E_WARNING | E_PARSE | E_NOTICE );
+
+#INCLUDES#
+include('includes/admin.class.php');
+include('includes/frontend.class.php');
+include('includes/settings.class.php');
+include('includes/resize.php');
+include('includes/functions.php');
+include('includes/wptooling.class.php');
+
+##INITIATE CLASSES##
+$settings = new floorplan_settings();
+$frontend = new floorplan_frontend();
+$admin = new admin_floorplan();
+$wp_tooling = new wp_tooling();
